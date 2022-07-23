399981dfd73f4c8de4dfa43f1ba9e050d4ea486c
atul <atulfalle007@gmail.com>
2022-07-23 12:52:37 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CommentComponent } from './comment/comment.component';
+import { CommentListingComponent } from './comment-listing/comment-listing.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CommentComponent,
+    CommentListingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/comment-listing/comment-listing.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CommentListingComponent } from './comment-listing.component';
+
+describe('CommentListingComponent', () => {
+  let component: CommentListingComponent;
+  let fixture: ComponentFixture<CommentListingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CommentListingComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CommentListingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
